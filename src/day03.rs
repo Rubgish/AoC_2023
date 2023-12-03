62ec28fbb06d8f2ece1e1d5c213a172b6ed8b03e
Matthew Parkes <mparkes@microsoft.com>
2023-12-03 11:12:34 +0000

    day3

--- src/day03.rs
+++ src/day03.rs
@@ -1,8 +1,76 @@
+use std::collections::HashMap;
+
 pub fn day03(input_lines: &str) -> (String, String) {
-    let _ = input_lines;
-    let answer1 = 0;
-    let answer2 = 0;
+    let mut a: [[char; 142]; 142] = [['.'; 142]; 142];
+    let mut lin_num = 0;
+    let mut nums: Vec<Num> = Vec::new();
+    for line in input_lines.lines() {
+        lin_num += 1;
+        let mut col_num = 1;
+        let mut in_num = false;
+        for c in line.chars() {
+            a[lin_num][col_num] = c;
+            if c.is_digit(10) && in_num == false {
+                in_num = true;
+                let new_num: i32 = line
+                    .chars()
+                    .skip(col_num - 1)
+                    .take_while(|c| c.is_digit(10))
+                    .collect::<String>()
+                    .parse()
+                    .unwrap();
+                nums.push(Num {
+                    num: new_num,
+                    pos: (lin_num as i32, col_num as i32),
+                });
+                // println!("{},{}: {}", lin_num, col_num, new_num);
+            } else if !c.is_digit(10) {
+                in_num = false;
+            }
+            col_num += 1;
+        }
+    }
+
+    // Look through the numbers and find any not . or digit
+    let mut gears: HashMap<(usize, usize), Vec<i32>> = HashMap::new();
+    let mut answer1 = 0;
+    for num in nums {
+        let len = num.num.to_string().len();
+        let mut found = false;
+        for x in num.pos.0 - 1..num.pos.0 + 2 {
+            for y in num.pos.1 - 1..num.pos.1 + len as i32 + 1 {
+                if a[x as usize][y as usize] != '.' && !a[x as usize][y as usize].is_digit(10) {
+                    if !found {
+                        answer1 += num.num;
+                    }
+                    found = true;
+                    if a[x as usize][y as usize] == '*' {
+                        gears
+                            .entry((x as usize, y as usize))
+                            .or_insert(Vec::new())
+                            .push(num.num);
+                    }
+                }
+            }
+        }
+    }
+
+    let mut answer2 = 0;
+    for (_, nums) in gears {
+        if nums.len() == 2 {
+            let mut a = 1;
+            for num in nums {
+                a *= num;
+            }
+            answer2 += a;
+        }
+    }
     (format!("{}", answer1), format!("{}", answer2))
+}
+
+struct Num {
+    num: i32,
+    pos: (i32, i32),
 }
 
 #[cfg(test)]
